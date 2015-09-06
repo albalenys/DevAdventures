bd0e9a5d2814ab07cc4c9f873fa8b5b96a1afa98
Albalenys <albalenysd@gmail.com>
2015-09-06 14:13:56 -0400

    add ability to create projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,9 +1,28 @@
 class ProjectsController < ApplicationController
+  include ApplicationHelper
+
   def index
     @projects = Project.all
   end
 
   def new
     @project = Project.new
+  end
+
+  def create
+    authorize_user
+    project = Project.new(project_params.merge(admin_id: session[:admin_id]))
+    if project.save
+      redirect_to projects_path
+    else
+      flash[:error] = "Invalid input: must include both title and url."
+      redirect_to new_project_path
+    end
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :url, :description)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -30,9 +30,9 @@ ActiveRecord::Schema.define(version: 20150906051252) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "title",       null: false
-    t.text     "description", null: false
-    t.text     "software",    null: false
     t.string   "url",         null: false
+    t.text     "description"
+    t.text     "software"
     t.integer  "admin_id",    null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
