84867719b30295e53f391fb146cf6eeff9148d5e
Abby <email": null>
2015-09-07 16:12:34 -0400

    add rspec tests for admin model

---
+++ spec/models/admin_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+require 'spec_helper'
+
+RSpec.describe Admin, type: :model do
+  it "has a valid factory" do
+    FactoryGirl.create(:admin).should be_valid
+  end
+
+  it "is invalid without a username" do
+    FactoryGirl.build(:admin, username: nil).should_not be_valid
+  end
+
+  it "is invalid without a password" do
+    FactoryGirl.build(:admin, password: nil).should_not be_valid
+  end
+
+  it "is invalid without a unique username" do
+    admin1 = FactoryGirl.create(:admin, username: "abby")
+    admin2 = FactoryGirl.build(:admin, username: "abby")
+    admin2.should_not be_valid
+  end
+
+  it "should have many posts" do
+    admin = Admin.reflect_on_association(:posts)
+    admin.macro.should == :has_many
+  end
+
+  it "should have many projects" do
+    admin = Admin.reflect_on_association(:projects)
+    admin.macro.should == :has_many
+  end
+end
