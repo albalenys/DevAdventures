e75d788e9d45199306a661ce01184846ae01df5f
Abby <email": null>
2015-09-16 18:38:30 -0400

    replace sqlite3 with postgresql for heroku

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150906051252) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "admins", force: :cascade do |t|
     t.string   "username",        null: false
     t.string   "password_digest", null: false
