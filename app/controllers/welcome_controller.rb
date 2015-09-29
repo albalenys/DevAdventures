e9782a59e6ac9bcf81e8831ab73c3afac3a5b676
Albalenys <albalenysd@gmail.com>
2015-09-29 01:25:23 -0400

    add projects to index page listing

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
   def index
-    @recent_posts = Post.all.order(created_at: :desc).last(2)
+    all_work = Post.all + Project.all
+    @recent_work = all_work.sort_by{ |work| work.created_at }.reverse.first(2)
+    #unable to use active record, no associations between posts and projects.
   end
 
   def search
