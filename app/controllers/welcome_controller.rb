175075334fa2521ba7b20b46859e687a6d478299
Albalenys <albalenysd@gmail.com>
2015-12-11 11:20:34 -0500

    add scrolling effect for welcome page and blog index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    all_work = Post.all + Project.all
-    @recent_work = all_work.sort_by{ |work| work.created_at }.reverse.first(2)
-    #unable to use active record, no associations between posts and projects.
+    @posts_by_month = Post.order(created_at: :desc).sort_by_month
   end
 
   def search
