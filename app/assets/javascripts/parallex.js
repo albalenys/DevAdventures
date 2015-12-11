6723332e79241d84c6d33ef6933775dba64232fb
Albalenys <albalenysd@gmail.com>
2015-12-11 13:50:04 -0500

    add blu effect to welcome div

--- app/assets/javascripts/parallex.js
+++ app/assets/javascripts/parallex.js
@@ -18,11 +18,26 @@ $(document).ready(function(){
 
   $(window).scroll(function (event) {
     var height = $(window).scrollTop();
+    var blur_effect = "blur(" + height/50 + "px)"
+
     if(height  > 680) {
       $("header").css("background-color", "#FFF");
     }
     else {
       $("header").css("background-color", "transparent");
+    }
+
+    if(height > 80) {
+      $("#welcome").addClass("blur")
+      $(".blur").css("-webkit-filter", blur_effect);
+      $(".blur").css("-moz-filter", blur_effect);
+      $(".blur").css("-o-filter", blur_effect);
+      $(".blur").css("-ms-filter", blur_effect);
+      $(".blur").css("filter", blur_effect);
+    }
+    else {
+      $("#welcome").removeClass("blur")
+      $("#welcome").removeAttr("style");
     }
   });
 });
