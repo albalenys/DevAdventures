f3acce58515a98956ac154c0beff50837450cb45
Albalenys <albalenysd@gmail.com>
2015-12-11 11:23:52 -0500

    add scrolling effect for projects and about page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    projects = Project.all.order(created_at: :desc)
+    @projects = projects.paginate(:page => params[:page], :per_page => 4)
     @posts_by_month = Post.order(created_at: :desc).sort_by_month
   end
 
