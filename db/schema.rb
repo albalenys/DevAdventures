2c8a937b9c80d83cca69f0363e6569231ab4359a
Albalenys <albalenysd@gmail.com>
2015-12-14 18:50:38 -0500

    remove comments from schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-ActiveRecord::Schema.define(version: 20150906051252) do
+ActiveRecord::Schema.define(version: 20151214234730) do
   enable_extension "plpgsql"
 
   create_table "admins", force: :cascade do |t|
@@ -18,12 +18,13 @@ ActiveRecord::Schema.define(version: 20150906051252) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "title",       null: false
-    t.string   "url",         null: false
+    t.string   "url"
     t.text     "description"
     t.string   "software"
     t.integer  "admin_id",    null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "github"
   end
 
   create_table "resources", force: :cascade do |t|
@@ -33,4 +34,5 @@ ActiveRecord::Schema.define(version: 20150906051252) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 end
