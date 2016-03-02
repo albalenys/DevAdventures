6f22eaed4b6a42744444706837e071e3604edbf3
Albalenys <albalenysd@gmail.com>
2016-03-02 13:24:05 -0500

    change 'next' and 'previous' posts to display only public posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,13 +3,13 @@ class PostsController < ApplicationController
   before_filter :find_post, except: [:new, :create, :private]
 
   def private
-    posts = Post.where(private: true)
-    @posts_by_month = posts.order(created_at: :desc).sort_by_month
+    private_posts = Post.where(private: true)
+    @posts_by_month = private_posts.order(created_at: :desc).sort_by_month
   end
 
   def show
-    @next_post = Post.find_by(id: @post.id + 1)
-    @previous_post = Post.find_by(id: @post.id - 1)
+    @next_post = @post.next_post("up")
+    @previous_post = @post.next_post("down")
     authorize_user if @post.private
   end
 
@@ -19,7 +19,6 @@ class PostsController < ApplicationController
 
   def create
     post = Post.new(post_params.merge(admin_id: session[:admin_id]))
-    binding.pry
     if post.save
       redirect_to "/#blogs"
     else
--- app/controllers/shared_controller.rb
+++ app/controllers/shared_controller.rb
@@ -2,8 +2,8 @@ class SharedController < ApplicationController
   def index
     projects = Project.all.order(created_at: :desc)
     @projects = projects.paginate(:page => params[:page], :per_page => 4)
-    posts = Post.where(private: false)
-    @posts_by_month = posts.order(created_at: :desc).sort_by_month
+    public_posts = Post.where(private: false)
+    @posts_by_month = public_posts.order(created_at: :desc).sort_by_month
   end
 
   def search
