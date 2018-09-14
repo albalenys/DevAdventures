1d3cb764af3b1e48e659bc52b4ffd06af9124751
Abby <abby.delossantos.contractor@macmillan.com>
2018-09-14 14:11:25 -0400

    Only show post edit link when hovered over

--- app/assets/javascripts/post-show.js
+++ app/assets/javascripts/post-show.js
@@ -1,3 +1,14 @@
+$('#post-list').ready(function() {
+  $("li[id^='post-']").hover(function(event) {
+    let parentElement = event.target.closest("li[id^='post-']");
+    editButton = $(parentElement).find('.sub-icon');
+    editButton.show();
+  },
+  function() {
+    editButton.hide();
+  });
+});
+
 $('#tag-list').ready(function() {
   $("span[id^='tag-']").hover(function(event) {
     let parentElement = event.target.closest("span[id^='tag-']");
