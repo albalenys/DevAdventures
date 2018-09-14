2b81d0684d5a4ecb22965cc0b62d1f669e04f241
Abby <abby.delossantos.contractor@macmillan.com>
2018-09-13 21:15:24 -0400

    Restructure css files; clean up css in _post

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,40 +1,38 @@
-$(document).ready(function() {
+$('#welcome').ready(function() {
   renderRandomBg();
   renderRandomColors();
 
-  if ($('#welcome').length) {
-    $("a[href*='#']").click(function() {
-      // Parallex reference: http://www.1stwebdesigner.com/create-scrolling-parallax-website/
-      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
-      && location.hostname == this.hostname) {
-        var $target = $(this.hash);
-        $target = $target.length && $target
-        || $('[name=' + this.hash.slice(1) +']');
-        if ($target.length) {
-          var targetOffset = $target.offset().top;
-          $('html,body')
-          .animate({scrollTop: targetOffset}, 1000);
-         return false;
-        }
+  $("a[href*='#']").click(function() {
+    // Parallex reference: http://www.1stwebdesigner.com/create-scrolling-parallax-website/
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+    && location.hostname == this.hostname) {
+      var $target = $(this.hash);
+      $target = $target.length && $target
+      || $('[name=' + this.hash.slice(1) +']');
+      if ($target.length) {
+        var targetOffset = $target.offset().top;
+        $('html,body')
+        .animate({scrollTop: targetOffset}, 1000);
+       return false;
       }
-    });
+    }
+  });
 
-    $(window).scroll(function() {
-      // Dynamically change blur effect (on welcome section) according to scroll position.
-      let height = $(window).scrollTop();
-      let blurEffect = `blur(${height/50}px)`;
+  $(window).scroll(function() {
+    // Dynamically change blur effect (on welcome section) according to scroll position.
+    let height = $(window).scrollTop();
+    let blurEffect = `blur(${height/50}px)`;
 
-      $(".blurScroll").css("-webkit-filter", blurEffect);
-      $(".blurScroll").css("-moz-filter", blurEffect);
-      $(".blurScroll").css("-o-filter", blurEffect);
-      $(".blurScroll").css("-ms-filter", blurEffect);
-      $(".blurScroll").css("filter", blurEffect);
-    });
+    $(".blurScroll").css("-webkit-filter", blurEffect);
+    $(".blurScroll").css("-moz-filter", blurEffect);
+    $(".blurScroll").css("-o-filter", blurEffect);
+    $(".blurScroll").css("-ms-filter", blurEffect);
+    $(".blurScroll").css("filter", blurEffect);
+  });
 
-    $("#welcome h1").click(function() {
-      renderRandomBg();
-    });
-  }
+  $("#welcome h1").click(function() {
+    renderRandomBg();
+  });
 });
 
 let renderRandomBg = function() {
