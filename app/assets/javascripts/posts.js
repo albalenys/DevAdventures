44bba1872275cabe1a9f072abdc7660f3771fb65
Abby <abby.delossantos.contractor@macmillan.com>
2018-09-17 16:29:29 -0400

    Rewrite posts.js to work dynamically

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,32 +1,38 @@
-$('#post-list').ready(function() {
-  $("li[id^='post-']").hover(function(event) {
-    let parentElement = event.target.closest("li[id^='post-']");
-    editButton = $(parentElement).find('.fa');
-    editButton.show();
-  },
-  function() {
-    editButton.hide();
-  });
+$('#posts-container').ready(function() {
+  $('#posts-container').on({
+    mouseenter: function(event) {
+      let parentElement = event.target.closest("li[id^='post-']");
+      editButton = $(parentElement).find('.fa');
+      editButton.show();
+    },
+    mouseleave: function() {
+      editButton.hide();
+    } 
+  }, "li[id^='post-']");
 });
 
 $('#tag-list').ready(function() {
-  $("span[id^='tag-']").hover(function(event) {
-    let parentElement = event.target.closest("span[id^='tag-']");
-    deleteButton = $(parentElement).find('.fa');
-    deleteButton.show();
-  },
-  function() {
-    deleteButton.hide();
-  });
+  $('#tag-list').on({
+    mouseenter: function(event) {
+      let parentElement = event.target.closest("span[id^='tag-']");
+      deleteButton = $(parentElement).find('.fa');
+      deleteButton.show();
+    },
+    mouseleave: function() {
+      deleteButton.hide();
+    } 
+  }, "span[id^='tag-']");
 });
 
 $('#resource-list').ready(function() {
-  $("li[id^='resource-']").hover(function(event) {
-    let parentElement = event.target.closest('li');
-    editLinks = $(parentElement).find('.edit-links');
-    editLinks.show();
-  },
-  function() {
-    editLinks.hide();
-  });
+  $('#resource-list').on({
+    mouseenter: function(event) {
+      let parentElement = event.target.closest("li[id^='resource-']");
+      editLinks = $(parentElement).find('.edit-links');
+      editLinks.show();
+    },
+    mouseleave: function() {
+      editLinks.hide();
+    } 
+  }, "li[id^='resource-']");
 });
