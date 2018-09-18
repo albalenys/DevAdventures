912a7e9c19926d9c7514d534bb6d2d712796cf18
Abby <abby.delossantos.contractor@macmillan.com>
2018-09-18 16:08:34 -0400

    Update project update and create to only rerender project item

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -15,10 +15,8 @@ class ProjectsController < ApplicationController
   end
 
   def create
-    project = Project.new(project_params.merge(admin_id: session[:admin_id]))
-    if project.save
-      ordered_projects = Project.all.order(created_at: :desc)
-      @projects = ordered_projects.paginate(page: params[:page], per_page: 4)
+    @project = Project.new(project_params.merge(admin_id: session[:admin_id]))
+    if @project.save
       respond_to :js
     else
       flash[:error] = 'Invalid input: must include both title and url.'
@@ -31,10 +29,7 @@ class ProjectsController < ApplicationController
     respond_to :js
   end
 
-  def update
-    ordered_projects = Project.all.order(created_at: :desc)
-    @projects = ordered_projects.paginate(page: params[:page], per_page: 4)
-    
+  def update    
     if @project.update_attributes(project_params)
       respond_to :js
     else
