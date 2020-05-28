73c9e7c081680004efd782b360f09ba91369ef70
Albalenys <albalenysd@gmail.com>
2020-05-27 21:07:04 -0400

    Update seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,4 @@
 # frozen_string_literal: true
 
-admin = Admin.create(username: 'Abby', password: 'password')
+Admin.create(username: 'Alba', password: 'password')
 
-30.times do
-  post = FactoryBot.create(:post)
-  project = FactoryBot.create(:project)
-
-  3.times do
-    post.tags.find_or_create_by(name: Faker::Book.genre)
-    post.resources.create(title: Faker::Book.title, url: Faker::Internet.url)
-    project.tags.find_or_create_by(name: Faker::Book.genre)
-  end
-end
